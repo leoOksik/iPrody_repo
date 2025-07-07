c4fc31d529c5ef39da19ffefc500f5ae5a6dda23
Oksana <oksleontev@yandex.ru>
2025-07-07 15:35:27 +0300

    Lesson 1. Homework

--- payment-service/simple-http-server/src/main/java/org/example/Main.java
+++ payment-service/simple-http-server/src/main/java/org/example/Main.java
@@ -3,31 +3,45 @@ package org.example;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
 
 public class Main {
+    private static final String MODULE_PATH = "simple-http-server";
+
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(8080);
-        System.out.println("Server started at http://localhost:8080");
-        while (true) {
+        System.out.println("Server started on port 8080");
+
+        while(true) {
             Socket clientSocket = serverSocket.accept();
-            BufferedReader in = new BufferedReader(new
-                    InputStreamReader(clientSocket.getInputStream()));
-            BufferedWriter out = new BufferedWriter(new
-                    OutputStreamWriter(clientSocket.getOutputStream()));
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
 
-            String line;
-            while ((line = in.readLine()) != null && !line.isEmpty()) {
-                System.out.println(line);
+            String request = in.readLine();
+            System.out.println("request: " + request);
+            String fileStr = request.substring(request.indexOf('/') + 1,request.indexOf(' ', request.indexOf('/')));
+            System.out.println("file: " + fileStr);
+
+            File file = new File(MODULE_PATH + "/static/" + fileStr);
+
+            if(file.exists() && file.isFile()) {
+
+                String contentType = Files.probeContentType(file.toPath());
+                out.write("HTTP/1.1 200 (OK)\r\n");
+                out.write("Content-Type: " + contentType + "; charset=UTF-8\r\n");
+                out.write("Content-Length: " + file.length() + "\r\n");
+                out.flush();
+
+                System.out.println("Content-Type: " + contentType);
+                System.out.println("Content-Length: " + file.length() + "bytes");
+                System.out.println("HTTP/1.1 200 (OK)");
             }
-
-            String response = "<h1>Hello from server!</h1>";
-            out.write("HTTP/1.1 200 OK\r\n");
-            out.write("Content-Type: text/html; charset=UTF-8\r\n");
-            out.write("Content-Length: " + response.length() + "\r\n");
-            out.write("\r\n");
-            out.write(response);
-            out.flush();
+            else {
+                out.write("HTTP/1.1 404 (Not Found)\r\n");
+                System.out.println("HTTP/1.1 404 (Not Found)");
+            }
             clientSocket.close();
         }
+
     }
 }
