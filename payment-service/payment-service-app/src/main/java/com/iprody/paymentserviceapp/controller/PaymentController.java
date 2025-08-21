15667aed95546e96469d8a6840f96aa3519b3fa5
Oksana <oksleontev@yandex.ru>
2025-08-21 16:50:39 +0300

    hw 13 (Spring Security + KeyCloak)

--- payment-service/payment-service-app/src/main/java/com/iprody/paymentserviceapp/controller/PaymentController.java
+++ payment-service/payment-service-app/src/main/java/com/iprody/paymentserviceapp/controller/PaymentController.java
@@ -3,25 +3,15 @@ package com.iprody.paymentserviceapp.controller;
 import com.iprody.paymentserviceapp.dto.PaymentDto;
 import com.iprody.paymentserviceapp.dto.PaymentNoteUpdateDto;
 import com.iprody.paymentserviceapp.persistence.PaymentFilterDTO;
+import com.iprody.paymentserviceapp.service.PaymentService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import com.iprody.paymentserviceapp.service.PaymentService;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -34,19 +24,22 @@ public class PaymentController {
 
     private final PaymentService paymentService;
 
-    @GetMapping("all")
+    @GetMapping("/all")
+    @PreAuthorize("hasAnyRole('admin', 'reader')")
     public List<PaymentDto> getPayments() {
         return paymentService.getPayments();
     }
 
     @GetMapping("/{guid}")
     @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasAnyRole('admin', 'reader')")
     public PaymentDto getPayment(@PathVariable UUID guid) {
         return paymentService.getPayment(guid);
     }
 
     @GetMapping("/search")
     @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasAnyRole('admin', 'reader')")
     public Page<PaymentDto> searchPayments(
         @ModelAttribute PaymentFilterDTO paymentFilter,
         @RequestParam(defaultValue = "0") int page,
@@ -74,24 +67,28 @@ public class PaymentController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasRole('admin')")
     public PaymentDto create(@RequestBody @Valid PaymentDto dto) {
         return paymentService.create(dto);
     }
 
     @PutMapping("/{guid}")
     @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasRole('admin')")
     public PaymentDto update(@PathVariable UUID guid, @RequestBody PaymentDto dto) {
         return paymentService.update(guid, dto);
     }
 
     @DeleteMapping("/{guid}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PreAuthorize("hasRole('admin')")
     public void delete(@PathVariable UUID guid) {
         paymentService.delete(guid);
     }
 
     @PatchMapping("/{guid}/note")
     @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasRole('admin')")
     public PaymentDto updateNote(@PathVariable UUID guid, @RequestBody @Valid PaymentNoteUpdateDto noteDto) {
         return paymentService.updateNote(guid, noteDto.getNote());
     }
